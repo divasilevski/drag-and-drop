d9fb4c7c721b3573f5c06079c99d969a80f8ae88
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-01-20 15:09:04 +0300

    Ужасный способ 2

--- src/components/DragAndDropBoard/Board.class.ts
+++ src/components/DragAndDropBoard/Board.class.ts
@@ -131,11 +131,13 @@ export class Board {
     let Y = e.pageY - rootRect.top - start.dy;
     const W = rootRect.width;
     const H = rootRect.height;
+    const DW = W / this.size.col;
+    const DH = H / this.size.row;
 
     if (X < 0) X = 0;
     if (Y < 0) Y = 0;
-    if (X > W) X = W - 1 - (W / this.size.col) * (size.col - 1);
-    if (Y > H) Y = H - 1 - (H / this.size.row) * (size.row - 1);
+    if (X > W - DW * size.col) X = W - 1 - DW * (size.col - 1);
+    if (Y > H - DH * size.row) Y = H - 1 - DH * (size.row - 1);
 
     const position = {
       x: Math.floor((X / W) * this.size.col),
@@ -144,7 +146,7 @@ export class Board {
 
     if (comparePositions(position, pos)) return null;
     if (this.hasTableKeys(position, size)) {
-      if (!this.canPush(key, position)) return null;
+      this.tryPush(key, position);
       return null;
     }
 
@@ -153,14 +155,20 @@ export class Board {
     return position;
   }
 
-  canPush(itemKey: string, pos: Position) {
-    const key = this.table[pos.y][pos.x];
-    if (!key) return;
+  tryPush(itemKey: string, pos: Position) {
+    const current = this.items[itemKey];
 
-    const first = this.items[itemKey];
-    const second = this.items[key];
+    // координаты которые нужно освободить '{ KEY' : [coords], ...}
+    const keys: any = {};
+    for (let i = 0; i < current.size.row; i++) {
+      for (let j = 0; j < current.size.col; j++) {
+        const key = this.table[i + pos.y][j + pos.x];
+        const coords = { x: j + pos.x, y: i + pos.y };
+        if (key) keys[key] = [...(keys[key] || []), coords];
+      }
+    }
 
-    const push = (newPos: Position) => {
+    const push = (newPos: Position, second: BoardItem) => {
       this.changeTable(second.pos!, second.size, null);
       if (!this.hasTableKeys(newPos, second.size)) {
         console.log("вниз");
@@ -171,10 +179,129 @@ export class Board {
       }
     };
 
-    push({ x: pos.x, y: pos.y + first.size.row });
-    push({ x: pos.x, y: pos.y - first.size.row });
-    push({ x: pos.x - first.size.col, y: pos.y });
-    push({ x: pos.x + first.size.col, y: pos.y });
+    // Проходимся по ключам виджетов
+    Object.keys(keys).forEach((key: string) => {
+      const widget = this.items[key];
+      if (
+        widget.size.col * widget.size.row === keys[key].length &&
+        current.size.col * current.size.row === keys[key].length
+      ) {
+        push(current.pos!, widget);
+      } else {
+        const widgetCenter = {
+          x: widget.pos!.x + widget.size.col / 2,
+          y: widget.pos!.y + widget.size.row / 2,
+        };
+        const currentCenter = {
+          x: pos.x + current.size.col / 2,
+          y: pos.y + current.size.row / 2,
+        };
+
+        const delta = [
+          widgetCenter.x - currentCenter.x,
+          widgetCenter.y - currentCenter.y,
+        ];
+
+        // зная дельту мы вычисляем приоритетное направление
+        // и кол-во клеток на которое нужно сдвинуть элемент
+
+        function pushLeft(x: number) {
+          const p = {
+            x: widget.pos!.x + x,
+            y: widget.pos!.y,
+          };
+          if (push(p, widget)) return true;
+          return false;
+        }
+        function pushRight(x: number) {
+          const p = {
+            x: widget.pos!.x + x,
+            y: widget.pos!.y,
+          };
+          if (push(p, widget)) return true;
+          return false;
+        }
+        function pushTop(y: number) {
+          const p = {
+            x: widget.pos!.x,
+            y: widget.pos!.y + y,
+          };
+          console.log(123);
+          if (push(p, widget)) return true;
+          return false;
+        }
+        function pushBot(y: number) {
+          const p = {
+            x: widget.pos!.x,
+            y: widget.pos!.y + y,
+          };
+          console.log(y);
+          if (push(p, widget)) return true;
+          return false;
+        }
+
+        function pushX() {
+          if (delta[0] > 0) {
+            if (pushLeft(delta[0] * 2)) return;
+            if (pushRight(delta[0] * 2 + current.size.col)) return;
+          } else {
+            if (pushRight(delta[0] * 2)) return;
+            if (pushLeft(delta[0] * 2 + current.size.col)) return;
+          }
+        }
+        function pushY() {
+          if (delta[1] > 0) {
+            if (pushTop(delta[1] * 2)) return;
+            if (pushBot(delta[1] * 2 + current.size.row)) return;
+          } else {
+            if (pushBot(delta[1] * 2)) return;
+            if (pushTop(delta[1] * 2 + current.size.row)) return;
+          }
+        }
+
+        if (Math.abs(delta[0]) > Math.abs(delta[1])) {
+          if (delta[0] > 0) {
+            if (pushLeft(delta[0] * 2)) return;
+            if (pushRight(delta[0] * 2 + current.size.col)) return;
+            pushY();
+          } else {
+            if (pushRight(delta[0] * 2)) return;
+            if (pushLeft(delta[0] * 2 + current.size.col)) return;
+            pushY();
+          }
+        } else {
+          if (delta[1] > 0) {
+            if (pushTop(delta[1] * 2)) return;
+            if (pushBot(delta[1] * 2 + current.size.row)) return;
+            pushX();
+          } else {
+            if (pushBot(delta[1] * 2)) return;
+            if (pushTop(delta[1] * 2 + current.size.row)) return;
+            pushX();
+          }
+        }
+      }
+    });
+
+    // console.log(current.pos);
+    this.showPattern(current.size, pos);
+
+    const side = {
+      l: -1,
+      r: 1,
+      t: -1,
+      b: 1,
+    };
+
+    // const key = this.table[pos.y][pos.x];
+    // if (!key) return;
+
+    // const second = this.items[key];
+
+    // push({ x: pos.x, y: pos.y + current.size.row });
+    // push({ x: pos.x, y: pos.y - current.size.row });
+    // push({ x: pos.x - current.size.col, y: pos.y });
+    // push({ x: pos.x + current.size.col, y: pos.y });
 
     return false;
   }
